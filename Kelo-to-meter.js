1966cd94258589182d4b593a0aac9c34374caa3b
ronykhan95 <farjilul.alam@gmail.com>
2021-01-23 04:21:18 +0600

    first commit

---
+++ Kelo-to-meter.js
@@ -0,0 +1,6 @@
+function KilometerToMeter(km){
+   var m = km * 1000;
+   return m;
+}
+var result = KilometerToMeter(100);
+console.log(result);
---
+++ hotelCost.js
@@ -0,0 +1,17 @@
+function hotelCost(totalDay) {
+    if( totalDay>=1 && totalDay<=10){
+        var taka = 100;
+        return totalDay * taka;
+    }
+    else if(totalDay>=11 && totalDay<=20){
+        var taka = 80;
+        return totalDay * taka;
+    }
+    else{
+        var taka = 50;
+        return totalDay * taka;
+
+    }
+}
+var result =  hotelCost(45);
+console.log(result);
